956cdca8d0e44c1fee286a14bd54a3b46db06e84
SteveGkor <stevegkortsopoulos@gmail.com>
2024-02-26 00:06:29 +0200

    final email template

--- services/email-serder-service.ts
+++ services/email-serder-service.ts
@@ -11,17 +11,20 @@ import Accountant from '../models/Accountant';
 export class EmailSenderService {
   private transporter: Transporter<SentMessageInfo>;
   private email: string;
+  private emailToSend: string;
 
   constructor() {
     this.transporter = nodemailer.createTransport({});
     this.email = '';
+    this.emailToSend = '';
   }
 
   private async initVariables() {
     const credentials = await Credential.findOne();
     const accountant = await Accountant.findOne();
 
-    this.email = accountant?.email as string;
+    this.email = credentials?.email as string;
+    this.emailToSend = accountant?.email as string;
     this.transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -32,12 +35,55 @@ export class EmailSenderService {
   }
 
   private async sendEmail(
-    to: string,
-    model: IExpense | IPurchase,
+    entity: IExpense | IPurchase,
     subject: EmailSubjects,
     images?: string[]
   ) {
-    const html = `<p>${model.description ?? 'Δεν υπαρχει description'}</p>`;
+    const formattedDate = entity.date
+      ? entity.date.toLocaleDateString('el-GR', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })
+      : 'Δεν προσδιορίζεται ημερομηνία';
+
+    const htmlTemplate = `
+    <html>
+      <head>
+        <style>
+          body { font-family: Arial, sans-serif; }
+          .header { background: #f3f3f3; padding: 20px; text-align: center; }
+          .content { margin: 20px; }
+          .footer { background: #f3f3f3; padding: 10px; text-align: center; }
+          img { max-width: 350px; margin: 20px; }
+        </style>
+      </head>
+      <body>
+        <div class="header">
+          <h2>${subject}</h2>
+        </div>
+        <div class="content">
+          <p>Περιγραφη: <b>${
+            entity.description ?? 'Δεν υπάρχει περιγραφή.'
+          }</b></p>
+          <p>Ημερομινία: <b>${formattedDate}</b></p>
+          <p>Τρόπος πληρωμής: <b>${entity.paymentMethod}</b></p>
+          ${
+            images && images.length > 0
+              ? '<div>' +
+                images
+                  .map((image) => `<img src="${image}" alt="Image">`)
+                  .join('') +
+                '</div>'
+              : '<p>Δεν βρέθηκαν εικόνες.</p>'
+          }
+        </div>
+        <div class="footer">
+          <p>Ευχαριστούμε.</p>
+        </div>
+      </body>
+    </html>`;
 
     const attachments = images?.map((image, index) => ({
       filename: `Image_${image.substring(image.lastIndexOf('/') + 1)}`,
@@ -47,9 +93,9 @@ export class EmailSenderService {
 
     const mailOptions = {
       from: this.email,
-      to,
+      to: this.emailToSend,
       subject,
-      html,
+      html: htmlTemplate,
       attachments,
     };
 
@@ -63,19 +109,13 @@ export class EmailSenderService {
   public async sendExpenses() {
     const expenses = await Expense.find({ isSend: false });
     await this.initVariables();
+
     for (const exp of expenses) {
-      const images = exp.images.filter(
-        (img) => img.link !== DefaultImage.EXPENSE_IMAGE
-      );
-
-      await this.sendEmail(
-        'stevegkortsopoulos@gmail.com',
-        exp,
-        EmailSubjects.EXPENSE,
-        images.map((img) => img.link)
-      );
-
+      const images = exp?.images
+        .filter((img) => img.link !== DefaultImage.EXPENSE_IMAGE)
+        .map((img) => img.link);
       const { _id } = exp;
+      await this.sendEmail(exp, EmailSubjects.EXPENSE, images);
       await this.updateEntity(_id, Expense);
     }
   }
@@ -84,43 +124,28 @@ export class EmailSenderService {
     const purchases = await Purchase.find({ isSend: false });
     await this.initVariables();
     for (const purch of purchases) {
-      await this.sendEmail(
-        'stevegkortsopoulos@gmail.com',
-        purch,
-        EmailSubjects.PURCHASE
-      );
-
       const { _id } = purch;
+      await this.sendEmail(purch, EmailSubjects.PURCHASE);
       await this.updateEntity(_id, Purchase);
     }
   }
 
   public async sendSingleExpense(id: string) {
     const expense = await Expense.findById(id);
+    const images = expense?.images
+      .filter((img) => img.link !== DefaultImage.EXPENSE_IMAGE)
+      .map((img) => img.link);
+
     await this.initVariables();
-    const images = expense?.images.filter(
-      (img) => img.link !== DefaultImage.EXPENSE_IMAGE
-    );
-
-    await this.sendEmail(
-      'stevegkortsopoulos@gmail.com',
-      expense as IExpense,
-      EmailSubjects.EXPENSE,
-      images?.map((img) => img.link)
-    );
-
+    await this.sendEmail(expense!, EmailSubjects.EXPENSE, images);
     await this.updateEntity(id, Expense);
   }
 
   public async sendSinglePurchase(id: string) {
     const purchase = await Purchase.findById(id);
-    await this.initVariables();
-    await this.sendEmail(
-      'stevegkortsopoulos@gmail.com',
-      purchase as IPurchase,
-      EmailSubjects.PURCHASE
-    );
 
+    await this.initVariables();
+    await this.sendEmail(purchase!, EmailSubjects.PURCHASE);
     await this.updateEntity(id, Purchase);
   }
 }
